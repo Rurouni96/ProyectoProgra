5a32f1fd8ac86ae22b42b3687d911782a5ccb817
Ruro2 <102768784+Rurouni96@users.noreply.github.com>
2022-04-07 10:38:10 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto/Models/PedidoDetalle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto.Models
+{
+    public class PedidoDetalle
+    {
+        [Key]
+        [Required]
+        public int PedidoDetalleId { get; set; }
+        [Required]
+        public int PedidoId { get; set; }
+        [Required]
+        public int ProductoId { get; set; }
+        [Required]
+        public decimal Precio { get; set; }
+        [Required]
+        public int Cantidad { get; set; }
+        [Required]
+        public decimal Descuento { get; set; }
+        [Required]
+        public decimal SubTotalLinea { get; set; }
+        [Required]
+        public decimal Total { get; set; }
+    }
+}
